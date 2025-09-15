424d113e53e7cf0e4fcdd3add1f8a851e84c29c0
Trebulkins <trebinivan2005@gmail.com>
2025-09-15 14:48:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nm_lab21/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.nm_lab21
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.nm_lab21.ui.theme.NM_lab21Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NM_lab21Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DemoScreen(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DemoScreen(modifier: Modifier = Modifier) {
+    Text(
+        "Hello Compose",
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NM_lab21Theme {
+        DemoScreen()
+    }
+}
